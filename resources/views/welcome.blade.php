3428e9a746a1ace45ceec98bd38c75a0f53d9fef
dptole <danillo.paiva.toledo@gmail.com>
2017-04-12 06:17:35 +0000

    - Initial commit.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+  </head>
+  <body>
+    <div class="flex-center position-ref full-height">
+      <div class="content">
+        <div class="title m-b-md">
+          This Laravel app is under development.
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
