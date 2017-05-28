1450f60935ecd50e59d131c93e8c4455c7593404
dptole <danillo.paiva.toledo@gmail.com>
2017-05-28 16:34:50 +0000

    - Do not forget to add bootstrap.js.

--- resources/views/partials/javascript.blade.php
+++ resources/views/partials/javascript.blade.php
@@ -1,3 +1,4 @@
 
 <script type="text/javascript" src="https://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+<script type="text/javascript" src="https://dptole.ngrok.io/laravel/r/js.js"></script>
 
