09d9ebb3fd5abb233ff74cbbe1710af9e5a2b86c
dptole <danillo.paiva.toledo@gmail.com>
2017-08-19 02:28:10 -0300

    - Add migration answers.

---
+++ database/migrations/2017_08_19_052517_answers_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::defaultStringLength(191);
+        Schema::create('answers', function(Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id')->unsigned();
+            $table->unsignedInteger('survey_id');
+            $table->unsignedInteger('question_id');
+            $table->unsignedInteger('question_option_id');
+            $table->timestamps();
+        });
+
+        Schema::table('answers', function(Blueprint $table) {
+            $table->foreign('survey_id')->references('id')->on('surveys')->onDelete('cascade');
+        });
+
+        Schema::table('answers', function(Blueprint $table) {
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+        });
+
+        Schema::table('answers', function(Blueprint $table) {
+            $table->foreign('question_option_id')->references('id')->on('questions_options')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answers', function(Blueprint $table) {
+            $table->dropForeign(['survey_id']);
+            $table->dropForeign(['question_id']);
+            $table->dropForeign(['question_option_id']);
+        });
+        Schema::dropIfExists('answers');
+    }
+}
