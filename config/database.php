fc4ce2f6b1afbfab74ed424b15a67a880fcb2c3c
Zeeshan Mehmood <zeeshan.epicstdio@gmail.com>
2019-12-18 23:53:54 +0500

    ATTR_EMULATE_PREPARES added in config/database.php

--- config/database.php
+++ config/database.php
@@ -52,6 +52,9 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'options'   => [
+                \PDO::ATTR_EMULATE_PREPARES => true
+            ]
         ],
 
         'pgsql' => [
