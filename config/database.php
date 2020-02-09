e1a03fb432ea7625d04d4c1c22a2fdb9a5fe2578
dptole <danillo.paiva.toledo@gmail.com>
2020-02-09 08:05:53 -0300

    - Prepare for tests with CircleCI.

--- config/database.php
+++ config/database.php
@@ -39,6 +39,12 @@ return [
             'prefix' => '',
         ],
 
+        'sqlite_testing' => [
+            'driver'   => 'sqlite',
+            'database' => storage_path().'/testing.sqlite',
+            'prefix'   => '',
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
